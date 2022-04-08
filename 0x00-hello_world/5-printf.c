a8dbb600a5b0d9facfa053eecced825832426c78
Elias Girma <eliasgirma.h@gmail.com>
2022-04-08 05:19:15 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+include <stdio.h>
+/**
+  *main -Enter point
+  *My hello, printf task
+  *Return: reture 1 after program execution
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
