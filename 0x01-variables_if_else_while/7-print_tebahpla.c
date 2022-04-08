7688402d159d439e62a5743b29b571184a8ca147
Elias Girma <eliasgirma.h@gmail.com>
2022-04-08 14:17:13 -0700

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,20 @@
-  2 #include <stdio.h>
-  3 /**
-  4  * main - Entry point
-  5  * Description: prints lowercase alphabets in reverse
-  6  * Return: Always 0 (success)
-  7  */
-  8 int main(void)
-  9 {
- 10         int a;
- 11
- 12         for (a = 'z'; a >= 'a'; a--)
- 13         {
- 14                 putchar(a);
- 15         }
- 16         putchar('\n');
- 17         return (0);
- 18 }
- 19 © 2022 G
+#include <stdio.h>
 
+/**
+* main - prints alphabet
+* Description: alphabet in reverse
+* Return: 0
+*/
+int main(void)
+{
+	char x = 'z';
+
+	while (x >= 'a')
+	{
+		putchar(x);
+		x--;
+	}
+
+	putchar('\n');
+	return (0);
+}
